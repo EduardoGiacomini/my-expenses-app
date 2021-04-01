dc0cefb904054101005ff47953c879475b6ba00e
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2021-03-31 22:19:03 -0400

    [REFACTOR] Diminuir tamanho do texto ao reduzir largura da tela

--- src/views/pages/components/Avatar.vue
+++ src/views/pages/components/Avatar.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex items-center space-x-3">
     <img class="w-12 h-12 rounded-full" :src="user.photo" :alt="$t('pages.components.avatar.alt')"/>
-    <div>
-      <p class="text-base font-medium">{{ user.name }}</p>
-      <p class="text-sm text-gray-500">{{ user.email }}</p>
+    <div class="overflow-hidden">
+      <p class="text-base font-medium truncate">{{ user.name }}</p>
+      <p class="text-sm text-gray-500 truncate">{{ user.email }}</p>
     </div>
   </div>
 </template>
