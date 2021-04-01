34e1fee8b03f684d2b8485c0a345137d5232ae3c
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2021-03-31 22:44:19 -0400

    [FEATURE] Criei caso de teste para o componente Avatar

---
+++ tests/unit/components/Avatar.spec.js
@@ -0,0 +1,27 @@
+import { shallowMount } from '@vue/test-utils'
+import i18n from '../../../src/commons/plugins/i18n'
+import Avatar from '../../../src/views/pages/components/Avatar'
+
+describe('Avatar.vue', () => {
+  let user = null
+
+  beforeAll(() => {
+    user = {
+      name: 'Carlos',
+      email: 'carloseduardo.diasgiacomini@gmail.com',
+      photo: 'photo.jpg'
+    }
+  })
+
+  test('Deve renderizar as props name e email na tela', () => {
+    const wrapper = shallowMount(Avatar, {
+      propsData: {
+        user
+      },
+      i18n
+    })
+
+    expect(wrapper.text()).toMatch(user.name)
+    expect(wrapper.text()).toMatch(user.email)
+  })
+})
